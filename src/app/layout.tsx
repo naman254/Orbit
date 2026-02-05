cba305c964713971191969761f7b05e439cd8a91
Naman  Singh <naman.singh.254@gmail.com>
2026-02-06 03:02:58 +0530

    03: database setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Mono, Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from '@clerk/nextjs'
-import { dark } from "@clerk/themes";
+import { Providers } from "@/components/providers";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -34,36 +25,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider
-      appearance={{ theme: dark }}
-    >
+    
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.variable} ${plexMono.variable} antialiased`}
       >
-        <ThemeProvider 
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <header>
-          <SignedOut>
-            <SignInButton/>
-            <SignUpButton>
-              <button className="bg-rose-500 text-white p-2 rounded">
-                Sign Up
-              </button>
-            </SignUpButton>
-          </SignedOut>
-          <SignedIn>
-            <UserButton/>
-          </SignedIn>
-          </header>
+        
+          <Providers>
           {children}
-        </ThemeProvider>
+          </Providers>
       </body>
     </html>
-    </ClerkProvider>
   );
 }
